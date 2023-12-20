632229b24c410c91f6ebd3bf23f39034197105ba
TheGrinchOnMath <jonathanwakeman@hotmail.com>
2023-12-20 15:43:42 +0100

    working on attempt to create the functionality i want

---
+++ test2/js/script.js
@@ -0,0 +1,20 @@
+/*
+this file contains the functions necessary to create the menus for selecting the factions
+there is also necessary padding to be added for the structure to make sense
+*/
+
+function generateBLUFOR() {
+    // remove all buttons from factions and eras, show the factions from BLUFOR
+}
+
+function generateOPFOR() {
+    //idem, but with OPFOR
+}
+
+function generateCIVILIAN() {
+    //ibidem, but with CIVILIAN
+}
+
+function generateINDEP() {
+    //ibididem, but with INDEP
+}
